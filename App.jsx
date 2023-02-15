1a439b9abefdf3bfa175d8d960b98a0d9dd99a00
higuchi <higuchi.timecapsule@gmail.com>
2023-02-15 10:12:09 +0900

    style memolist screen

--- App.jsx
+++ App.jsx
@@ -1,17 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello bang>World</Hello>
-      <Hello style={{ fontSize: 16 }}>Small World</Hello>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
+
+      {/* アップバー */}
+      <View style={styles.appbar}>
+        <View style={styles.appbarInner}>
+          <Text style={styles.appbarTitle}>MemoApp</Text>
+          <Text style={styles.appbarRight}>ログアウト</Text>
+        </View>
+      </View>
+
+      {/* メモリスト */}
+      <View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
+          </View>
+          <View>
+            <Text style={styles.memoListItemDelete}>×</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
+          </View>
+          <View>
+            <Text style={styles.memoListItemDelete}>×</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
+          </View>
+          <View>
+            <Text style={styles.memoListItemDelete}>×</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonlabel}>+</Text>
+      </View>
     </View>
   );
 }
@@ -19,8 +56,86 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F0F4F8',
+  },
+
+  appbar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#467FD3',
+    justifyContent: 'flex-end',
+  },
+
+  appbarInner: {
     alignItems: 'center',
+  },
+
+  appbarRight: {
+    position: 'absolute',
+    right: 19,
+    bottom: 16,
+    color: 'rgba(255,255,255, 0.8)',
+  },
+
+  appbarTitle:{
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+
+  memoListItem:{
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.15)',
+  },
+
+  memoListItemTitle:{
+    fontSize: 16,
+    lineHeight: 32,
+  },
+
+  memoListItemDate:{
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+
+  memoListItemDelete:{
+    fontSize: 25,
+    color: 'rgba(0, 0, 0, 0.4)',
+  },
+
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
     justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 25,
+    bottom: 40,
+
+    // IOSしか対応していない
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+
+    // Andoroidしか対応していない
+    elevation: 8,
+  },
+
+  circleButtonlabel: {
+    color: '#ffffff',
+    fontSize: 30,
+    lineHeight: 30,
   },
 });
